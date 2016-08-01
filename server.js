92a345d838db1d103a3a5989761b5f78a826ffec
Chris Harvey <charvey77@gmail.com>
2016-07-31 22:05:43 -0400

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ var express = require('express');
 // Create our app
 var app = express();
 
+// get the port from Heroku, or set it to 3000 manually
+const PORT = process.env.PORT||3000;
+
+app.use(function(req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 // which folder do we want to serve?
 app.use(express.static('public'));
 
 // start the server
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
 });
